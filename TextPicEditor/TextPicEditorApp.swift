05536f34d0e4cca4ff5b1e77da9748d9b9e98dbe
development30years <comddol@naver.com>
2024-12-03 20:33:06 +0900

    Initial Commit

---
+++ TextPicEditor/TextPicEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextPicEditorApp.swift
+//  TextPicEditor
+//
+//  Created by nextaoi on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TextPicEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TextPicEditorTests/TextPicEditorTests.swift
@@ -0,0 +1,16 @@
+//
+//  TextPicEditorTests.swift
+//  TextPicEditorTests
+//
+//  Created by nextaoi on 12/3/24.
+//
+
+import Testing
+
+struct TextPicEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
